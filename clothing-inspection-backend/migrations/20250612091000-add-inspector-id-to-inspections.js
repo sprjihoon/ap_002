f4e6678ba90e4b89cc9d1771f33030b53e269f1f
sprjihoon <vovok112@gmail.com>
2025-06-24 21:56:57 +0900

    Update 20250612091000-add-inspector-id-to-inspections.js

--- clothing-inspection-backend/migrations/20250612091000-add-inspector-id-to-inspections.js
+++ clothing-inspection-backend/migrations/20250612091000-add-inspector-id-to-inspections.js
@@ -5,8 +5,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('inspections', 'inspector_id', {
       type: Sequelize.INTEGER,
-      allowNull: true
-      // PlanetScale 호환을 위해 FK 제거
+      allowNull: true,
+      // PlanetScale does not support foreign keys
     });
   },
 
