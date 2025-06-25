35723dc639e49fa09579497677e24a7cd7d8f4e6
sprjihoon <vovok112@gmail.com>
2025-06-25 11:45:24 +0900

    Update inspectionComment.js

--- clothing-inspection-backend/models/inspectionComment.js
+++ clothing-inspection-backend/models/inspectionComment.js
@@ -10,6 +10,14 @@ const InspectionComment = sequelize.define('InspectionComment', {
     type: DataTypes.INTEGER,
     allowNull: true
   },
+  inspectionId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
   createdAt: {
     type: DataTypes.DATE,
     allowNull: false,
