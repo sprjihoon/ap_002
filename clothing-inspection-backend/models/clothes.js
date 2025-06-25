19fa5d853cd01615ec915dd967f5c5a91601b08c
sprjihoon <vovok112@gmail.com>
2025-06-25 12:26:33 +0900

    Update clothes.js

--- clothing-inspection-backend/models/clothes.js
+++ clothing-inspection-backend/models/clothes.js
@@ -1,4 +1,4 @@
-+ const { DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Clothes = sequelize.define('Clothes', {
