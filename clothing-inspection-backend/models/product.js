5a908492eeec74cefa4593293abde53af1dbbe04
sprjihoon <vovok112@gmail.com>
2025-06-26 13:38:23 +0900

    Update product.js

--- clothing-inspection-backend/models/product.js
+++ clothing-inspection-backend/models/product.js
@@ -1,67 +1,77 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+// models/product.js
 
-const Product = sequelize.define('Product', {
-  company: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  productName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  size: {
-    type: DataTypes.JSON,
-    allowNull: true
-  },
-  color: {
-    type: DataTypes.JSON,
-    allowNull: true
-  },
-  wholesaler: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  wholesalerProductName: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  location: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW
-  }
-}, {
-  timestamps: false,
-  tableName: 'products'
-});
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    'Product',
+    {
+      company: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      productName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      size: {
+        type: DataTypes.JSON,
+        allowNull: true
+      },
+      color: {
+        type: DataTypes.JSON,
+        allowNull: true
+      },
+      wholesaler: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      wholesalerProductName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      }
+    },
+    {
+      tableName: 'products',
+      timestamps: false
+    }
+  );
 
-const ProductVariant = sequelize.define('ProductVariant', {
-  productId: {
+  const ProductVariant = sequelize.define(
+    'ProductVariant',
+    {
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      size: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      color: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      barcode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      }
+    },
+    {
+      tableName: 'product_variants',
+      timestamps: false
+    }
+  );
 
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  size: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  color: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  barcode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  }
-}, {
-  timestamps: false,
-  tableName: 'product_variants'
-});
+  // 관계 정의는 models/index.js에서 수행
 
-module.exports = { Product, ProductVariant };
+  return { Product, ProductVariant };
+};
