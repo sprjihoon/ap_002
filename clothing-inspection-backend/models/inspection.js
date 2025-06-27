81737efb66658e8b2b427dec9ae17565aa8ea478
Jihoon Jang <vovok112@gmail.com>
2025-06-27 16:44:51 +0900

    fix(model): remove circular import in Inspection to restore association loading

--- clothing-inspection-backend/models/inspection.js
+++ clothing-inspection-backend/models/inspection.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const { ProductVariant } = require('../models');
 
 const Inspection = sequelize.define('Inspection', {
   inspectionName: {
