872c4e9e503e37301d6c812f55b94d585fb7a5bf
Jihoon Jang <vovok112@gmail.com>
2025-06-27 16:16:19 +0900

    chore: update userRoutes

--- clothing-inspection-backend/models/inspection.js
+++ clothing-inspection-backend/models/inspection.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const { ProductVariant } = require('../models');
 
 const Inspection = sequelize.define('Inspection', {
   inspectionName: {
